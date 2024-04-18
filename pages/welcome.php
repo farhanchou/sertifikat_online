0e93812e228983785923c147986a78a09004e242
farhanchou <mfarhanfadillah468@gmail.com>
2024-04-18 18:48:38 +0700

    3

---
+++ pages/welcome.php
@@ -0,0 +1,31 @@
+<!-- welcome.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+    <div class="video-container">
+        
+
+        <div class="overlay"></div>
+
+        <div class="content">
+            <h2>Selamat Datang di Sertifikat Online</h2>
+            
+            <div class="container">
+                <div class="logo-container">
+                    <img src="../assets/images/lor.jpg" alt="Logo">
+                </div>
+                
+                <p>Silakan pilih opsi login:</p><br>
+                
+                <ul>
+                    <li><a href="../user/login_user.php">Login sebagai Pengguna (User)</a></li>
+                    <li><a href="../admin/login_admin.php">Login sebagai Admin</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ sertifikat_online.sql
@@ -0,0 +1,184 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 18 Apr 2024 pada 13.40
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sertifikat_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admins`
+--
+
+CREATE TABLE `admins` (
+  `admin_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `admins`
+--
+
+INSERT INTO `admins` (`admin_id`, `username`, `password`, `full_name`, `email`, `created_at`) VALUES
+(8, 'admin1', '$2y$10$iGChWp6nA1mk1x8GoTBxeeoM8UzgODb9sL/nQHkT8.fFC1iRiXK42', 'admin1', 'admin@gmail.com', '2024-04-17 00:55:33');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `certificates`
+--
+
+CREATE TABLE `certificates` (
+  `certificate_id` int(11) NOT NULL,
+  `participant_name` varchar(255) NOT NULL,
+  `event_name` varchar(255) NOT NULL,
+  `event_date` date NOT NULL,
+  `certificate_text` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `certificate_assignments`
+--
+
+CREATE TABLE `certificate_assignments` (
+  `assignment_id` int(11) NOT NULL,
+  `certificate_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `event_id` int(11) DEFAULT NULL,
+  `assigned_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `role` enum('user','admin') NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `events`
+--
+
+CREATE TABLE `events` (
+  `event_id` int(11) NOT NULL,
+  `event_name` varchar(255) NOT NULL,
+  `event_date` date NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `organizer` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indeks untuk tabel `certificates`
+--
+ALTER TABLE `certificates`
+  ADD PRIMARY KEY (`certificate_id`);
+
+--
+-- Indeks untuk tabel `certificate_assignments`
+--
+ALTER TABLE `certificate_assignments`
+  ADD PRIMARY KEY (`assignment_id`),
+  ADD KEY `certificate_id` (`certificate_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `admin_id` (`admin_id`),
+  ADD KEY `event_id` (`event_id`);
+
+--
+-- Indeks untuk tabel `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `certificates`
+--
+ALTER TABLE `certificates`
+  MODIFY `certificate_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT untuk tabel `certificate_assignments`
+--
+ALTER TABLE `certificate_assignments`
+  MODIFY `assignment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `events`
+--
+ALTER TABLE `events`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
